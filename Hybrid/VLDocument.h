aa30652e2ead6d29022f7d2161ccb097b26967aa
jeffreyvarner <jeff@curveos.com>
2013-08-05 11:48:42 -0400

    Initial Commit

---
+++ Hybrid/VLDocument.h
@@ -0,0 +1,13 @@
+//
+//  VLDocument.h
+//  Hybrid
+//
+//  Created by Jeffrey Varner on 8/5/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VLDocument : NSDocument
+
+@end
---
+++ Hybrid/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Hybrid
+//
+//  Created by Jeffrey Varner on 8/5/13.
+//  Copyright (c) 2013 Varnerlab. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
